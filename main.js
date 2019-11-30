92eb7e864e2ec16a2ef8d708932d7fe079373cd4
Wendy Munyasi <wendymunyasi@gmail.com>
2019-11-30 12:53:56 +0300

    add function toggleDatePicker to main.js.

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const date_picker_element = document.querySelector('.date-picker');
 const selected_date_element = document.querySelector('date-picker .selected-date');
 const dates_element = document.querySelector('.date-picker .dates');
+
 date_picker_element.addEventListener('click', toggleDatePicker);
+
+function toggleDatePicker (e) {
+    dates_element.classList.toggle('active')
+}
