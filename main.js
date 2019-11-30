4c6457e81d5b484341fac0e844b88a296c5583aa
Wendy Munyasi <wendymunyasi@gmail.com>
2019-11-30 12:48:46 +0300

    add 'event listener' to 'main.js'

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 const date_picker_element = document.querySelector('.date-picker');
-const selected_date_element = document.querySelector('date-picker .selected-date')
-const dates_element = document.querySelector('.date-picker .dates')
+const selected_date_element = document.querySelector('date-picker .selected-date');
+const dates_element = document.querySelector('.date-picker .dates');
+date_picker_element.addEventListener('click', toggleDatePicker);
