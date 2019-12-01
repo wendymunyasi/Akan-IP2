b7548e43e4f960896fa99ce1f0795d52014c73fb
Wendy Munyasi <wendymunyasi@gmail.com>
2019-12-02 02:20:41 +0300

    add a getInputFunction to js.

---
+++ main2.js
@@ -0,0 +1,10 @@
+var century, year, month, dayOfMonth, dayOfWeek, day;
+
+function getInput() {
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    dayOfMonth = parseInt(document.getElementById("monthday").value);
+}
+
+if(century)
