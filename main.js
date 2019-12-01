96aca065ddacc4f6c674f32beb8153f400b56797
Wendy Munyasi <wendymunyasi@gmail.com>
2019-12-01 20:14:28 +0300

    add height to .days in css.

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 const date_picker_element = document.querySelector('.date-picker');
-const selected_date_element = document.querySelector('date-picker .selected-date');
+const selected_date_element = document.querySelector('.date-picker .selected-date');
 const dates_element = document.querySelector('.date-picker .dates');
 
 date_picker_element.addEventListener('click', toggleDatePicker);
 
 function toggleDatePicker (e) {
-    dates_element.classList.toggle('active')
+    console.log(e.path);
+    if (!checkEventPathForClass(e.path, 'dates')) {
+    dates_element.classList.toggle('active');
+    }
 }
 
 function checkEventPathForClass (path, selector) {
     for (let i = 0; i < path; i++) {
-        if (path [i].classList| && path[i].classList.contains(selector)) {
+        if (path [i].classList && path[i].classList.contains(selector)) {
             return true;
-        } 
+        }
     }
-    return false
-}
+    return false;
+};
+
+
+
