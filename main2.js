dc4dcafd91f2f59415d153fc72d222813909e514
Wendy Munyasi <wendymunyasi@gmail.com>
2019-12-02 02:34:58 +0300

    add checkDayOfTheWeek and calculationOfDayOfTheWeek functions to js.

--- main2.js
+++ main2.js
@@ -7,4 +7,35 @@ function getInput() {
     dayOfMonth = parseInt(document.getElementById("monthday").value);
 }
 
-if(century)
+if(century == "") {
+    alert("Not Applicable");
+    return false;
+} else if(year == "") {
+    alert("Not Applicable");
+    return false;
+} else if(century == "") {
+    alert("Not Applicable");
+    return false;
+} else if(dayofMonth == "") {
+    alert("Not Applicable");
+    return false;
+}
+
+function calculationOfDayOfTheWeek() {
+    getInput();
+    dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
+    console.log(dayOfWeek);
+    return (Math.floor(dayOfWeek));
+    if (dayOfWeek < 0) {
+      dayOfWeek = dayOfWeek * -1;
+    }
+    else if (dayOfWeek > 0) {
+      return dayOfWeek;
+    }
+}
+
+function checkDayOfWeek(){
+    day = calculationOfDayOfTheWeek();
+     checkGender();
+     console.log("The function runs");
+}
