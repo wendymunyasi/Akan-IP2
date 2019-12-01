a9dd634173dc9fc4630254999bdfdda38a7fa3cc
Wendy Munyasi <wendymunyasi@gmail.com>
2019-12-01 23:32:25 +0300

    all.

---
+++ main.js
@@ -0,0 +1,33 @@
+const date_picker_element = document.querySelector('.date-picker');
+const selected_date_element = document.querySelector('.date-picker .selected-date');
+const dates_element = document.querySelector('.date-picker .dates');
+const mth_element = document.querySelector('.date-picker .dates .month .mth');
+const next_mth_element = document.querySelector('.date-picker .dates .month .next-mth');
+const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
+
+
+
+
+
+
+date_picker_element.addEventListener('click', toggleDatePicker);
+
+//FUNCTIONS
+function toggleDatePicker (e) {
+    console.log(e.path)
+    if (!checkEvenPathForClass(e.path, 'dates')) {
+    dates_element.classList.toggle('active')
+    }
+}
+
+//HELPER FUNCTIONS
+function checkEvenPathForClass (path, selector) {
+    for (let i = 0; i < path; i++) {
+        if (path[i].classList && path[i].classList.contains(selector)) {
+            return true;
+        }
+        
+    }
+    
+    return false;
+}
