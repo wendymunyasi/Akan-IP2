37cec44c539e8a8c32d23c40863e364f3a47a55e
Wendy Munyasi <wendymunyasi@gmail.com>
2019-12-02 07:26:06 +0300

    add heading h3 to index.html.

--- main.js
+++ main.js
@@ -1,134 +1,56 @@
-const date_picker_element = document.querySelector('.date-picker');
-const selected_date_element = document.querySelector('.date-picker .selected-date');
-const dates_element = document.querySelector('.date-picker .dates');
-const mth_element = document.querySelector('.date-picker .dates .month .mth');
-const next_mth_element = document.querySelector('.date-picker .dates .month .next-mth');
-const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
-const days_element = document.querySelector('.date-picker .dates .days');
+//declaring arrays
+var daysOfWeek=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var maleNames=[
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+];
 
-let date = new Date();
-let day = date.getDate ();
-let month = date.getMonth();
-let year = date.getFullYear();
+var femaleNames=[
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+];
 
-let selectedDate = date;
-let selectedDay = day;
-let selectedMonth = month;
-let selectedYear = year;
+//calling functions
+function getName(){
+var year=parseInt(document.getElementById("year").value);
+var month =parseInt(document.getElementById("month").value);
+var day=parseInt(document.getElementById("day").value);
+var male = document.getElementById("male");
+var female = document.getElementById("female");
 
-mth_element.textContent = months[month] + ' ' + year;
+//validation
+if(day<=0||day>31)
+alert("Oops!please enter a valid day");
+else if(month<0||month>12)
+alert("Oops!please enter a valid month");
 
-selected_date_element.textContent = formatDate(date);
 
-populateDates();
- 
+var day = new Date(year + "/" + month + "/" + day);
 
-
-date_picker_element.addEventListener('click', toggleDatePicker);
-next_mth_element.addEventListener('click', goToNextMonth);
-prev_mth_element.addEventListener('click', goToPrevMonth);
-
-//FUNCTIONS
-function toggleDatePicker (e) {
-    console.log(e.path)
-    if (!checkEvenPathForClass(e.path, 'dates')) {
-    dates_element.classList.toggle('active')
-    }
+var birthDay=day.getDay();
+if(male.checked==true){
+   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + maleNames[birthDay])
 }
-
-function goToNextMonth (e) {
-    month++;
-    if (month > 11) {
-        month=0;
-        year++;
-    }
-    mth_element.textContent = months[month] + ' ' + year;
-    populateDates();
+else if(female.checked==true){
+   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + femaleNames[birthDay])
 }
-
-function goToPrevMonth (e) {
-    month--;
-    if (month < 0) {
-        month = 11;
-        year--;
-    }
-    mth_element.textContent = months[month] + ' ' + year;
-    selected_date_element.dataset.value = selectedDate;
-
-    populateDates();
-}
-
-function populateDates (e) {
-    days_element.innerHTML = '';
-    let amount_days = 31;
-
-    if(month == 1) {
-        amount_days = 28;
-    } else if (month == 3) {
-        amount_days = 30;
-    } else if (month == 5) {
-        amount_days = 30;
-    } else if (month == 10) {
-        amount_days = 30;
-    } else {
-        amount_days = 31;
-    }
-
-
-    for (let i = 0; i < amount_days; i++) {
-        const day_element = document.createElement('div');
-        day_element.classList.add ('day');
-        day_element.textContent = i + 1;
-
-        if (selectedDay == (i + 1) && selectedYear == year && selectedMonth == month) {
-            day_element.classList.add('selected');
-        }
-
-        day_element.addEventListener('click', function() {
-            selectedDate = new Date(year + '-' + (month + 1) + '-' + (i+1));
-            selectedDay = (i + 1);
-            selectedMonth = month;
-            selectedYear = year;
-
-            selected_date_element.textContent = formatDate(selectedDate);
-            selected_date_element.dataset.value = selectedDate;
-
-            populateDates();
-        });
-        
-
-        days_element.appendChild(day_element);
-
-    }
-}
-
-
-//HELPER FUNCTIONS
-function checkEvenPathForClass (path, selector) {
-    for (let i = 0; i < path; i++) {
-        if (path[i].classList && path[i].classList.contains(selector)) {
-            return true;
-        }
-        
-    }
-    
-    return false;
-}
-
-function formatDate (d) {
-    let day = d.getDate();
-    if (day < 10) {
-        day = '0' + day;
-    }
-
-    let month = d.getMonth() + 1;
-    if (month < 10) {
-        month = '0' + month;
-    }
-
-    let year = d.getFullYear();
-
-    return day + ' / ' + month + ' / ' + year;
 }
